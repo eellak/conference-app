5712c172a7721bf154b8ad1302efda1ea2c09562
LeoLoupos <leo.loupos@gmail.com>
2017-10-11 20:01:44 +0300

    v.74

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,10 @@ import { ConferenceApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { PopoverPage } from '../pages/about-popover/about-popover';
-import { AccountPage } from '../pages/account/account';
-import { LoginPage } from '../pages/login/login';
 import { MapPage } from '../pages/map/map';
 import { SchedulePage } from '../pages/schedule/schedule';
 import { ScheduleFilterPage } from '../pages/schedule-filter/schedule-filter';
 import { SessionDetailPage } from '../pages/session-detail/session-detail';
-import { SignupPage } from '../pages/signup/signup';
 import { SpeakerDetailPage } from '../pages/speaker-detail/speaker-detail';
 import { SpeakerListPage } from '../pages/speaker-list/speaker-list';
 import { TabsPage } from '../pages/tabs-page/tabs-page';
@@ -39,13 +36,11 @@ import {UniqueDeviceID} from "@ionic-native/unique-device-id";
   declarations: [
     ConferenceApp,
     AboutPage,
-    AccountPage,
     MapPage,
     PopoverPage,
     SchedulePage,
     ScheduleFilterPage,
     SessionDetailPage,
-    SignupPage,
     SpeakerDetailPage,
     SpeakerListPage,
     TabsPage,
@@ -66,10 +61,7 @@ import {UniqueDeviceID} from "@ionic-native/unique-device-id";
         { component: MapPage, name: 'Map', segment: 'map' },
         { component: AboutPage, name: 'About', segment: 'about' },
         { component: TutorialPage, name: 'Tutorial', segment: 'tutorial' },
-        { component: SupportPage, name: 'SupportPage', segment: 'support' },
-        { component: LoginPage, name: 'LoginPage', segment: 'login' },
-        { component: AccountPage, name: 'AccountPage', segment: 'account' },
-        { component: SignupPage, name: 'SignupPage', segment: 'signup' }
+        { component: SupportPage, name: 'SupportPage', segment: 'support' }
       ]
     }),
     AngularFireModule.initializeApp(FIREBASE_CREDENTIALS),
@@ -80,13 +72,11 @@ import {UniqueDeviceID} from "@ionic-native/unique-device-id";
   entryComponents: [
     ConferenceApp,
     AboutPage,
-    AccountPage,
     MapPage,
     PopoverPage,
     SchedulePage,
     ScheduleFilterPage,
     SessionDetailPage,
-    SignupPage,
     SpeakerDetailPage,
     SpeakerListPage,
     TabsPage,
