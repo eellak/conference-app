ec68c7a7bb7fdbd16c0be53b8d8fc59657f0813c
Leonidas Loupos <leo.loupos@gmail.com>
2017-10-27 22:21:11 +0300

    Update firebase.credentials.ts

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,3 @@
 export const FIREBASE_CREDENTIALS = {
-  apiKey: "AIzaSyBlppTelvdXUA_lTpHa8T15HOMsrVyqa_k",
-  authDomain: "fosscomm-2017-ellak.firebaseapp.com",
-  databaseURL: "https://fosscomm-2017-ellak.firebaseio.com",
-  projectId: "fosscomm-2017-ellak",
-  storageBucket: "fosscomm-2017-ellak.appspot.com",
-  messagingSenderId: "891419208695"
+
 };
