d01b683db8b76d2af8dd04927614fc0d0e97598f
LeoLoupos <leo.loupos@gmail.com>
2017-10-27 22:19:58 +0300

    v5.01
    v5.01

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CREDENTIALS = {
-  apiKey: "AIzaSyCcpUEpZCdmEGMrnVgo0a9miLf-URv2fME",
-  authDomain: "fosscomm-2017-11cb0.firebaseapp.com",
-  databaseURL: "https://fosscomm-2017-11cb0.firebaseio.com",
-  projectId: "fosscomm-2017-11cb0",
-  storageBucket: "",
-  messagingSenderId: "33660622244"
+  apiKey: "AIzaSyBlppTelvdXUA_lTpHa8T15HOMsrVyqa_k",
+  authDomain: "fosscomm-2017-ellak.firebaseapp.com",
+  databaseURL: "https://fosscomm-2017-ellak.firebaseio.com",
+  projectId: "fosscomm-2017-ellak",
+  storageBucket: "fosscomm-2017-ellak.appspot.com",
+  messagingSenderId: "891419208695"
 };
